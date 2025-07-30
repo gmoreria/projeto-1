2f587ab4c05c346c9ad15d60a90d080e23fb18a8
gmoreria <g.pereira02@escola.pr.gov.br>
2025-07-30 12:18:56 -0300

    ds

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
 const perguntas = [
     {
-        enunciado: "",
+        enunciado: " As redes sociais devem ser usadas principalmente para entretenimento ou para educação?",
         alternativas: [
             {
-                texto:"",
-                afirmação: ""
+                texto:"a",
+                afirmação: "Para entretenimento"
             },
             {
-                texto: "",
-                afirmação: ""
+                texto: "b",
+                afirmação: "Para educação"
             }
         ]
     },
     {
-        enunciado: "repete o padrao superior para todas perguntas",
-    }
-]
+        enunciado: "Você acredita que as redes sociais ajudam a criar comunidades ou promovem o isolamento ?:",
+            {
+                texto:"a",
+                afirmação: "Criam comunidades"
+            },
+            {
+                texto: "b",
+                afirmação: "Promovem isolamneto"
+            }
+        ]{
+        enunciado: " O conteúdo compartilhado pode ser extremamente positivo ao disseminar informações úteis, onde causas sociais, iniciativas de solidariedade e conhecimento em dioversaas áreas?",
+        alternativas: [
+            {
+                texto:"a",
+                afirmação: "Impacto negativo"
+            },
+            {
+                texto: "b",
+                afirmação: "|Impacto posistivo"
+            }
+        ]{
+        enunciado: " As redes sociais devem ser mais regulamentadas ou permanecerem livres?",
+        alternativas: [
+            {
+                texto:"a",
+                afirmação: "Devem ser regulamentadas"
+            },
+            {
+                texto: "b",
+                afirmação: "Permanecerem livres"
+            }
+
+        enunciado: " A influência das redes sociais na autoestima da pessoas é mais positiva ou negativa?",
+        alternativas: [
+            {
+                texto:"a",
+                afirmação: "Para entretenimento"
+            },
+            {
+                texto: "b",
+                afirmação: "Para educação"
+            }
+        ]
+        ]
